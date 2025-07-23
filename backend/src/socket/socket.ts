c6fc0d853e55ffbc072ed72a1003703d84de7fe2
Sangeeth Ramesh <sangeethramesh05@gmail.com>
2025-07-23 15:58:50 +0530

    socket url changed

--- backend/src/socket/socket.ts
+++ backend/src/socket/socket.ts
@@ -17,7 +17,7 @@ interface MessageData {
 export const initializeSocket = (server: HttpServer, chatService: IChatService) => {
   const io = new Server(server, {
     cors: {
-      origin: ["http://localhost:5173", "http://localhost:3000"],
+      origin:  process.env.FRONTEND_URL as string,
       methods: ["GET", "POST"],
       credentials: true,
     },
